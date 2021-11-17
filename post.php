f622178807577fa453e0e6d6ff96547f55a834d9
hnr-2318 <90799728+hnr-2318@users.noreply.github.com>
2021-11-17 11:12:32 -0500

    Updates to View Post

--- post.php
+++ post.php
@@ -1,29 +1,39 @@
  <!-- URL format /post.php?postId=<number> -->
 <?php include("./common/header.php") ?>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-7 text-center mb-5"><br>
+            <!-- Get posts from database -->
+          <h4>  <?php
+                // Connect to DB
+                require __DIR__ . './functions.php';
+                $conn = connectToDatabase();
+                $conn->select_db("blogs");
 
-<!-- Get posts from database -->
-<?php
-    // Connect to DB
-    require __DIR__ . './functions.php';
-    $conn = connectToDatabase();
-    $conn->select_db("blogs");
+            $postId = filter_input(INPUT_GET,'postId',FILTER_VALIDATE_INT);
+            if ($postId == NULL || $postId == FALSE) {
+                $postId = 1;
+            }
+            //---------Perform a query------------------------ 
+            // Sql injection Fix todo, neeed to allow performQuery to support placeholders
+            $sql = "SELECT * FROM posts where p_Id={$postId}"; 
+            $retval = performQuery($conn,$sql);
+                
+            while($row = $retval->fetch_assoc()) 
+            {
+                echo "<h2>{$row['title']} </h2>";
+                echo "{$row['date']} <hr>";
+                echo "{$row['text']} <br/>".
+                    "--------------------------------<br/><br/>";
+            } 
 
-   $postId = filter_input(INPUT_GET,'postId',FILTER_VALIDATE_INT);
-   if ($postId == NULL || $postId == FALSE) {
-       $postId = 1;
-   }
-   //---------Perform a query------------------------ 
-   // Sql injection Fix todo, neeed to allow performQuery to support placeholders
-   $sql = "SELECT * FROM posts where p_Id={$postId}"; 
-   $retval = performQuery($conn,$sql);
-    
-   while($row = $retval->fetch_assoc()) 
-   {
-      echo "{$row['text']} <br/>".
-         "--------------------------------<br/><br/>";
-   } 
-   $conn->close();
-?>
-
+            $conn->close();
+            ?>
+            
+            </h4>
+        </div>
+    </div>
+</div>
+<?php echo file_get_contents("viewPost.html"); ?>
 
 <?php echo file_get_contents("./common/footer.html"); ?>
