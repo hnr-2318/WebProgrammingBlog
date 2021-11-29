81e37c9e0c13ba7bc5a4977f0f021c721a11b4b1
Dillon <dillon.rawlins@gmail.com>
2021-11-28 22:43:20 -0500

    remove about nav link

--- common/header.php
+++ common/header.php
@@ -66,9 +66,6 @@
                                         <li class="nav-item">
                                             <a class="nav-link" href="./index.php"> Home </a>
                                         </li>
-                                        <li class="nav-item">
-                                            <a class="nav-link" href="#contact">About</a>
-                                        </li>
                                         <?php 
                                             if ($username !== "") {
                                                 echo 
