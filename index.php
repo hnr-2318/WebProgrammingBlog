b989bf8ecdf1901c7186c1b32ec480f5965cc64d
hnr-2318 <90799728+hnr-2318@users.noreply.github.com>
2021-12-03 18:11:05 -0500

    minor changes to appearance

--- index.php
+++ index.php
@@ -54,13 +54,9 @@
                                 <p>It is a long established fact that a reader will be distracted by the readable
                                     content of a page when looking at its layout. The point of using Lorem Ipsum is that
                                     it has a more-or-less normal distribution of letters,</p>
-                                <a href="#">Read More</a>
                             </div>
                         </div>
                         <div class="col-xl-4 col-lg-4 col-md-4 col-12-3">
-                            <div class="text-img">
-                                <figure><img src="images/box_img.png" alt="#" /></figure>
-                            </div>
                         </div>
                     </div>
                 </div>
